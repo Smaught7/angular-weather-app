e89d8ce831924864c7de307aba7d528769d3d92f
mateusz.jama <mateusz.jama@codefusion.pl>
2023-06-27 20:40:38 +0200

    fix

--- weather-app/src/app/search-city/search-city.component.ts
+++ weather-app/src/app/search-city/search-city.component.ts
@@ -4,7 +4,7 @@ import { WeatherService } from '../weather.service';
 @Component({
   selector: 'app-search-city',
   templateUrl: './search-city.component.html',
-  styleUrls: ['./search-city.component.sass'],
+  styleUrls: ['./search-city.component.scss'],
 })
 export class SearchCityComponent {
   @Output() newItemEvent = new EventEmitter<string>();
